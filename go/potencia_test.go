2961c168ed8d9112af8b2fd47ba11edadf7b5cd6
danielfireman-ifal <daniel.fireman@ifal.edu.br>
2021-11-24 11:41:33 +0000

    testes de integração da rota potência

--- go/potencia_test.go
+++ go/potencia_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -20,5 +24,25 @@ func TestPotencia_erro(t *testing.T) {
 	t.Run("expoenteInválido", func(t *testing.T) {
 		_, err := potencia("2", "b")
 		assert.Error(t, err)
+	})
+}
+
+func TestPotenciaHandler(t *testing.T) {
+	app := fiber.New()
+	registerHandlers(app)
+
+	t.Run("Ok", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/potencia/2/3", nil)
+		resp, err := app.Test(req, 1)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+		body, _ := io.ReadAll(resp.Body)
+		assert.Equal(t, "8.00", string(body))
+	})
+	t.Run("Error", func(t *testing.T) {
+		req := httptest.NewRequest("GET", "/potencia/2/aa", nil)
+		resp, _ := app.Test(req, 1)
+		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
 	})
 }
